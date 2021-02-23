ac50fa52a2ac26c908db323bd80f574b1dc54c90
Felicia Goh <minhui.felicia@gmail.com>
2021-02-23 16:06:27 +0800

    Add orders model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_075944) do
+ActiveRecord::Schema.define(version: 2021_02_23_080410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "slot_id", null: false
+    t.string "delivery_method"
+    t.string "delivery_address"
+    t.string "status"
+    t.float "total"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["slot_id"], name: "index_orders_on_slot_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -52,6 +66,8 @@ ActiveRecord::Schema.define(version: 2021_02_23_075944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "slots"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
   add_foreign_key "slots", "products"
 end
