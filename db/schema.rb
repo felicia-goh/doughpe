465d4180d924fabb1a5689315a0bca34523898df
Felicia Goh <minhui.felicia@gmail.com>
2021-02-23 16:17:45 +0800

    Add purchase model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_080834) do
+ActiveRecord::Schema.define(version: 2021_02_23_081049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2021_02_23_080834) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_purchases_on_order_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -78,6 +85,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_080834) do
   add_foreign_key "orders", "slots"
   add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
+  add_foreign_key "purchases", "orders"
   add_foreign_key "reviews", "orders"
   add_foreign_key "slots", "products"
 end
