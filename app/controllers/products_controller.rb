8334841d8ed63fb38c06834d98a251d6b239cfe6
Felicia Goh <minhui.felicia@gmail.com>
2021-02-24 17:59:19 +0800

    Add multiple slots in new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,22 @@
 class ProductsController < ApplicationController
   def new
     # pundit: only allow if user is a baker
-    @baker = current_user
     @product = Product.new
     @slot = Slot.new
   end
 
   def create
-    @baker = current_user
     @product = Product.new(strong_params)
     @product.user = current_user
-    @slot = Slot.new(strong_params_slots)
-    @slot.time_period = 'unselected'
+    quantity = strong_params_slots['available_quantity']
+    dates = strong_params_slots['date'].select { |d| d != '' }
     if @product.save
-      @slot.product = @product
-      @slot.save
+      dates.each do |date|
+        @slot = Slot.new(available_quantity: quantity, date: date)
+        @slot.product = @product
+        @slot.time_period = 'unselected'
+        @slot.save
+      end
       redirect_to root_path
     else
       render :new
@@ -28,6 +30,6 @@ class ProductsController < ApplicationController
   end
 
   def strong_params_slots
-    params.require(:slot).permit(:available_quantity)
+    params.require(:slot).permit(:available_quantity, date: [])
   end
 end
