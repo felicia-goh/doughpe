7d9f842c5ebafb159b99497e40b10660e8d0be81
Felicia Goh <minhui.felicia@gmail.com>
2021-02-24 10:55:45 +0800

    Add baker column to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_081049) do
+ActiveRecord::Schema.define(version: 2021_02_24_024923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,8 +76,8 @@ ActiveRecord::Schema.define(version: 2021_02_23_081049) do
     t.string "username"
     t.string "name"
     t.text "bio"
-    t.string "role"
     t.string "address"
+    t.boolean "baker", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
