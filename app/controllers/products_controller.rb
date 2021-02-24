040c3d8b34932e25011f4fcf858fd50980363fa7
Felicia Goh <minhui.felicia@gmail.com>
2021-02-24 14:47:20 +0800

    Create view for adding new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,25 @@
 class ProductsController < ApplicationController
   def new
+    # pundit: only allow if user is a baker
+    @baker = current_user
+    @product = Product.new
   end
 
   def create
+    @baker = current_user
+    @product = Product.new(strong_params)
+    @product.user = current_user
+
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:product).permit(:name, :price, :description)
   end
 end
