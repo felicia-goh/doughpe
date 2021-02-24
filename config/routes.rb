674fe41b278648fa77bbada7cbac877032d06adc
Felicia Goh <minhui.felicia@gmail.com>
2021-02-24 14:31:24 +0800

    Generate products controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'sign_up' }
   get 'bakers', to: 'users#index'
+  resources :products, only: [:new, :create]
 end
