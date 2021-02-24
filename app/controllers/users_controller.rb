5e167b884c526f62379865ef4520d9d82f8d8c2c
Deepana Naidu <deepana.naidu@gmail.com>
2021-02-24 14:35:08 +0800

    Add dotenv - Protect my secret data in .env file

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(strong_edit_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def strong_edit_params
+    params.require(:user).permit(:name, :bio, :address, :photo)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'sign_up' }
+  resources :users, only:[:edit, :update]
 
 end
