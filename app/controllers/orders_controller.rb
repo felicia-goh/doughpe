3e7798a420d332977e3c709329b9b53c2349f78d
christian quah <ChristianQuah.git@gmail.com>
2021-02-24 21:14:43 +0800

    added methods to the OrdersController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,19 @@
 class OrdersController < ApplicationController
   def new
+    @user = current_user
+    @order = Order.create(order_params)
+    @order.user = @user
+    @product = Product.find(params.require(:order).permit(:product_id)[:product_id].to_i)
+    # @order.product = @product
     raise
   end
 
+  private
+
+  def order_params
+    params.require(:order).permit(:quantity, :delivery_method, :delivery_address)
+  end
+
+  def slot_params
+  end
 end
