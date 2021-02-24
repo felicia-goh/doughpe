feac208f675e14f1b87e6284c9ea9307032a6b9c
Deepana Naidu <deepana.naidu@gmail.com>
2021-02-24 20:28:42 +0800

    had to re-do routes for edit profile in order for it to work

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'sign_up' }
   get 'bakers', to: 'users#index'
   resources :products, only: [:new, :create,:edit, :update]
+  resources :users, only:[:edit, :update]
 end
