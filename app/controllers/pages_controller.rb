c7b2b12c389e6068729085096b812cfe3fe2c1df
Felicia Goh <minhui.felicia@gmail.com>
2021-02-25 19:27:49 +0800

    implement simple search on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
   def home
-    @bakers = User.where(baker: true)
-    @products = Product.all
+    if params[:query].present?
+      @bakers = User.where("lower(username) LIKE ? AND baker= ?", "%#{params[:query].downcase}%", true)
+    else
+      @bakers = User.where(baker: true)
+    end
+
+    if params[:query].present?
+      @products = Product.where("lower(name) LIKE ?", "%#{params[:query].downcase}%")
+    else
+      @products = Product.all
+    end
   end
 end
