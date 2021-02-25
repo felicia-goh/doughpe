eff8a080a4b1fbe7222345de36c6e250ab74385a
christian quah <ChristianQuah.git@gmail.com>
2021-02-25 16:09:18 +0800

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'sign_up' }
   get '/bakers/:name', to: 'users#show_shop', as: 'baker'
   get 'bakers', to: 'users#index'
+  get '/orders', to: 'orders#index', as: 'orders'
   get '/orders/new', to: 'orders#new'
   post '/orders', to: 'orders#new'
   get '/sales', to: 'orders#sales'
   resources :products, only: [:new, :create, :edit, :update, :destroy]
   resources :users, only:[:edit, :update]
-  resources :orders, only: [:index, :new, :edit, :update]
+  resources :orders, only: [:new, :edit, :update]
 end
