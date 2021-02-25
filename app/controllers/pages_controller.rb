e3d088a456cc01c8a04e5e24e0346cd892cc8db4
Felicia Goh <minhui.felicia@gmail.com>
2021-02-25 22:54:02 +0800

    Show simple search results on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,12 @@
 class PagesController < ApplicationController
   def home
-    if params[:query].present?
-      @bakers = User.where("lower(username) LIKE ? AND baker= ?", "%#{params[:query].downcase}%", true)
-    else
-      @bakers = User.where(baker: true)
-    end
+    @bakers = User.where(baker: true)
+    @products = Product.all
 
     if params[:query].present?
-      @products = Product.where("lower(name) LIKE ?", "%#{params[:query].downcase}%")
-    else
-      @products = Product.all
+      @query = params[:query]
+      @bakers_results = User.where("lower(username) LIKE ? AND baker= ?", "%#{params[:query].downcase}%", true)
+      @products_results = Product.where("lower(name) LIKE ?", "%#{params[:query].downcase}%")
     end
   end
 end
