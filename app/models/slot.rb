c8241198a242acb3f42712354d6835683ab78710
Felicia Goh <minhui.felicia@gmail.com>
2021-02-26 12:23:28 +0800

    remove validation for slot:time_period, fixed add-product form, fixed show_shop if no product image

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -2,5 +2,5 @@ class Slot < ApplicationRecord
   belongs_to :product
   has_many :orders, dependent: :destroy
   validates :available_quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :time_period, inclusion: { in: %w[morning afternoon evening unselected] }
+
 end
