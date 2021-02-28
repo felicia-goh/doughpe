96dc42d0331573a896b1079fc2bf08702999ced2
Felicia Goh <minhui.felicia@gmail.com>
2021-03-01 00:07:46 +0800

    Add realistic seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_034100) do
+ActiveRecord::Schema.define(version: 2021_02_28_153609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(version: 2021_02_26_034100) do
   create_table "baskets", force: :cascade do |t|
     t.boolean "completed"
     t.float "total"
-    t.bigint "users_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["users_id"], name: "index_baskets_on_users_id"
+    t.index ["user_id"], name: "index_baskets_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
